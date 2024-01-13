783478d13454af11bb978cb83baf7c449c05f93c
mgKang3646 <lordofkangs@naver.com>
2024-01-13 14:00:35 +0900

    [섹션1] 로그추적기 V1 - 적용

---
+++ src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,41 @@
+package hello.advanced.trace;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+
+@Getter
+public class TraceId {
+
+    private String id; // 트랜잭션 ID
+    private int level; // 깊이
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0,8);
+    }
+
+    private TraceId createNextId(){
+        return new TraceId(id,level+1); //ID는 동일한데, 레벨은 동일해야 하므로
+    }
+
+    public TraceId createPreviousId(){
+        return new TraceId(id,level-1);
+    }
+
+    public boolean isFirstLevel(){
+        return level == 0;
+    }
+
+
+}
